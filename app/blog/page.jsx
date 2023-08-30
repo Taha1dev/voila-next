27e4728960b170530e9f3b062c64beb3f6d843c6
yasser_voila <yasser@voila.digital>
2023-08-30 11:11:06 +0300

    creating initial structure [Pages,Components]

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,10 +1,16 @@
 
+<<<<<<< HEAD
 const Blogs = async () => {
   return (
     <div>
       <h1>Users</h1>
     </div>
   );
+=======
+
+const Blogs = () => {
+  return <div>Blogs</div>;
+>>>>>>> 9c1aa71 (creating initial structure [Pages,Components])
 };
 
 export default Blogs;
--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import profilePic from "../public/girl.jpg";
 import Image from "next/image";
@@ -9,3 +10,14 @@ const Services = () => {
   );
 };
 export default Services;
+=======
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div>Services</div>
+  )
+}
+
+export default Services
+>>>>>>> 9c1aa71 (creating initial structure [Pages,Components])
