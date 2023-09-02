1cba9b98c5ab9f6e315e5fa81a8ecacb5935b9b8
yasser_voila <yasser@voila.digital>
2023-09-02 10:44:57 +0300

    blog main page

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,16 +1,11 @@
 
-<<<<<<< HEAD
-const Blogs = async () => {
-  return (
-    <div>
-      <h1>Users</h1>
-    </div>
-  );
-=======
 
 const Blogs = () => {
   return <div>Blogs</div>;
+<<<<<<< HEAD
 >>>>>>> 9c1aa71 (creating initial structure [Pages,Components])
+=======
+>>>>>>> 0efc93c (blog main page)
 };
 
 export default Blogs;
