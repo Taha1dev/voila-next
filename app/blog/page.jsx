74ef02ea731e69d00eea477dd4323a1301cb9f0f
yasser_voila <yasser@voila.digital>
2023-09-02 10:36:42 +0300

    add base pages

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,11 +1,23 @@
 
+<<<<<<< HEAD
 
 const Blogs = () => {
   return <div>Blogs</div>;
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> 9c1aa71 (creating initial structure [Pages,Components])
 =======
 >>>>>>> 0efc93c (blog main page)
+=======
+=======
+const Blogs = async () => {
+  return (
+    <div>
+      <h1>Users</h1>
+    </div>
+  );
+>>>>>>> a77c82f (add base pages)
+>>>>>>> 9087cea (add base pages)
 };
 
 export default Blogs;
--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,4 +1,7 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> a77c82f (add base pages)
 import React from "react";
 import profilePic from "../public/girl.jpg";
 import Image from "next/image";
@@ -10,6 +13,7 @@ const Services = () => {
   );
 };
 export default Services;
+<<<<<<< HEAD
 =======
 import React from 'react'
 
@@ -20,4 +24,10 @@ const Services = () => {
 }
 
 export default Services
+<<<<<<< HEAD
 >>>>>>> 9c1aa71 (creating initial structure [Pages,Components])
+=======
+>>>>>>> 9c1aa718c358a1c27adb1b14a2fa49c5b43f1df8
+=======
+>>>>>>> a77c82f (add base pages)
+>>>>>>> 9087cea (add base pages)
